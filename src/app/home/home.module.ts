9a5200c628209780ab53863a45190a85e41fe639
Ayush Vij <ayushvij@live.in>
2021-06-21 03:38:12 -0700

    Added everything until experiences

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +18,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  
 })
 export class HomePageModule {}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { AppAvailability } from '@ionic-native/app-availability/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [InAppBrowser, AppAvailability]
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private inAppBrowser: InAppBrowser,
+    private appAvailability: AppAvailability
+  ) { }
 
-}
+
+  onClick(event) {
+ 
+    if (event.detail.checked) {
+      document.body.setAttribute('data-theme', 'dark');
+    }
+    else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }}
